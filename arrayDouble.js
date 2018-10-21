3a20eb3b6ca4be0980185cd299630d39429d3242
James Bell <Noxachron@gmail.com>
2018-10-21 12:24:15 -0400

    Array.from is what I've been looking for(well 1 thing anyway)

--- arrayDouble.js
+++ arrayDouble.js
@@ -7,11 +7,12 @@ function doubleArray(arrayToBeDoubled){
         return arrayToBeDoubled;
     }
     //general case here
-     //need to double the first index, of the new sliced array,
+     //need to double the first index, of the new sliced array, combine first index with all the nested calls.
      arrayToBeDoubled[0] *=2;
-    return arrayToBeDoubled[0] + "," + doubleArray(arrayToBeDoubled.slice(1));
+    return Array.from([arrayToBeDoubled[0], ...doubleArray(arrayToBeDoubled.slice(1))]);//works with & without ...
 }
 
 let testArray = [1, 2, 3, 4, 5];
 // Goal: result [2, 4, 6, 8 ,10];
-console.log("result is: " + doubleArray(testArray));
+console.log("result is: " + doubleArray(testArray));// 2,4,6,8,10
+console.log(Array.isArray(doubleArray(testArray)));// true, cool
